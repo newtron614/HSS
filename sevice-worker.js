3d4961aeecc8948a87f6c957efa4453aa75cf196
newtron614 <newtron614@hotmail.com>
2025-05-20 13:36:45 +0900

    Add files via upload

---
+++ sevice-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pain-survey-v1';
+const urlsToCache = [
+  './05171200test4.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) { return cache.addAll(urlsToCache); })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
